08212ffd15cc6f231ac417a9266ece5fa094f867
Michael Caley <mjcaley@darkarctic.com>
2017-08-07 14:19:54 -0400

    Added nullptr checks in WeakPtr

--- gc_playground/gc.hpp
+++ gc_playground/gc.hpp
@@ -170,7 +170,7 @@ namespace GC
         
         T* operator->()
         {
-            return ptr->ptr;
+            return ptr->ptr.get();
         }
         
         ValueObject<T>* get_pointer() { return ptr; }
@@ -184,13 +184,44 @@ namespace GC
     template<typename T>
     struct WeakRef
     {
-        WeakRef(ValueObject<T>* ptr) : ptr(ptr) { ptr->add_weak_reference(); }
-        WeakRef(const WeakRef& ref) : ptr(ref.ptr) { ptr->add_weak_reference(); }
-        ~WeakRef() { ptr->del_weak_reference(); }
+        WeakRef() : ptr(nullptr) {};
+        WeakRef(ValueObject<T>* ptr) : ptr(ptr)
+        {
+            if (ptr)
+            {
+                ptr->add_weak_reference();
+            }
+        }
+        
+        WeakRef(const WeakRef& ref) : ptr(ref.ptr)
+        {
+            if (ptr)
+            {
+                ptr->add_weak_reference();
+            }
+        }
+        
+        ~WeakRef()
+        {
+            if (ptr)
+            {
+                ptr->del_weak_reference();
+            }
+        }
 
         operator Ref<T>() { return get_reference(); }
         
-        Ref<T> get_reference() const { return Ref<T>(ptr); }
+        explicit operator bool() { return has_reference(); }
+        
+        Ref<T> get_reference() const
+        {
+            if (!ptr)
+            {
+                throw std::bad_weak_ptr();
+            }
+            return Ref<T>(ptr);
+        }
+        bool has_reference() const { return ptr != nullptr; }
         
     private:
         ValueObject<T>* ptr;
