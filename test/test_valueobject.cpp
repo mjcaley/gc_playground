92fa2530e2f1e567555ce2f54b1f09dab71120bd
Michael Caley <mjcaley@darkarctic.com>
2017-08-13 13:19:55 -0400

    Got rid of tests to check the type, since we're using a strongly typed language Added test for nullptr being more explicit

--- test/test_valueobject.cpp
+++ test/test_valueobject.cpp
@@ -10,9 +10,7 @@ TEST_CASE( "ValueObject constructor", "[gc][valueobject]")
 {
     GC::ValueObject<int> v(42);
 
-    auto type_result = std::is_same<decltype(v.ptr), const std::unique_ptr<int>>::value;
-    REQUIRE(type_result);
-    REQUIRE(v.ptr);
+    REQUIRE(v.ptr != nullptr);
     REQUIRE(*v.ptr == 42);
 }
 
@@ -20,8 +18,6 @@ TEST_CASE( "ValueObject new_object", "[gc][valueobject]" )
 {
     auto v = GC::new_object<int>(42);
 
-    auto type_result = std::is_same<decltype(v), std::unique_ptr<GC::ValueObject<int>>>::value;
-    REQUIRE(type_result);
-    REQUIRE(v->ptr);
+    REQUIRE(v->ptr != nullptr);
     REQUIRE(*v->ptr == 42);
 }
