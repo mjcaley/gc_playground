f99c4b189ce28ada85c6dd7b40e2645e541dec58
Michael Caley <mjcaley@darkarctic.com>
2017-09-15 13:33:14 -0400

    TypedObject now has all members public

--- include/GC/GC/Object.hpp
+++ include/GC/GC/Object.hpp
@@ -2,51 +2,35 @@
 
 #include <type_traits>
 
-// #include "GC/RefFwd.hpp"
-
 
 namespace GC
 {
-    class Ptr;
-
-    template<typename T, typename Enable> struct Ref;
-
     struct Object {
         virtual ~Object() = default;
     };
 
     template<typename T, typename Enable = void>
-    class TypedObject : public Object {};
+    struct TypedObject : public Object {};
 
     template<typename T>
-    class TypedObject<T, std::enable_if_t<!std::is_array<T>::value>> : public Object
+    struct TypedObject<T, std::enable_if_t<!std::is_array<T>::value>> : public Object
     {
-    public:
         using type = T;
 
         template<typename ... Param>
         TypedObject(Param ... params) : object(T(params ...)) {}
 
-    private:
         T object;
-
-        friend Ptr;
-        friend Ref<T, typename std::enable_if_t<!std::is_array<T>::value>>;
     };
 
     template<typename T>
-    class TypedObject<T, std::enable_if_t<std::is_array<T>::value>> : public Object
+    struct TypedObject<T, std::enable_if_t<std::is_array<T>::value>> : public Object
     {
-    public:
         using type = construct_array_t<T>;
 
         template<typename ... Param>
         TypedObject(Param ... params) : object({ params ... }) {}
 
-    private:
         construct_array_t<T> object;
-
-        friend Ptr;
-        friend Ref<T, std::enable_if_t<std::is_array<T>::value>>;
     };
 }
