f85635eb3a634fd22126cee86e6e6ed33aeb19cb
Michael Caley <mjcaley@darkarctic.com>
2017-09-18 16:41:27 -0400

    Added static keywords to Collector functions and variables

--- include/GC/GC/Collector.hpp
+++ include/GC/GC/Collector.hpp
@@ -13,11 +13,11 @@ namespace GC
 {
     namespace Detail
     {
-        unsigned int current_mark;
-        std::forward_list<Ptr> used_list;
+        static unsigned int current_mark;
+        static std::forward_list<Ptr> used_list;
     }
 
-    struct Allocations
+    static struct Allocations
     {
         std::uintmax_t existing { 0 };
         std::uintmax_t created { 0 };
@@ -34,12 +34,11 @@ namespace GC
         }
     } allocations;
 
-    float collection_percentage { 0.25 };
+    static float collection_percentage { 0.25 };
 
-    struct Ptr;
-    void collect();
+    static void collect();
 
-    Ptr* add(Ptr pointer)
+    static Ptr* add(Ptr pointer)
     {
         Detail::used_list.emplace_front(std::move(pointer));
         ++allocations.created;
@@ -52,7 +51,7 @@ namespace GC
         return &Detail::used_list.front();
     }
 
-    void mark(unsigned int marker)
+    static void mark(unsigned int marker)
     {
         for(auto& ptr : Detail::used_list)
         {
@@ -63,7 +62,7 @@ namespace GC
         }
     }
 
-    void sweep(unsigned int marker)
+    static void sweep(unsigned int marker)
     {
         Detail::used_list.remove_if(
             [&marker](auto& ptr) {
@@ -77,7 +76,7 @@ namespace GC
         );
     }
 
-    void collect()
+    static void collect()
     {
         mark(Detail::current_mark);
         sweep(Detail::current_mark);
