2b24c76fd9bbd69b97674d6905124ab765419db7
Michael Caley <mjcaley@darkarctic.com>
2017-09-18 13:55:16 -0400

    Eliminated specialized TypedObject

--- include/GC/GC/Object.hpp
+++ include/GC/GC/Object.hpp
@@ -9,11 +9,8 @@ namespace GC
         virtual ~Object() = default;
     };
 
-    template<typename T, typename Enable = void>
-    struct TypedObject : public Object {};
-
     template<typename T>
-    struct TypedObject<T, std::enable_if_t<!std::is_array<T>::value>> : public Object
+    struct TypedObject : public Object
     {
         using type = T;
 
@@ -21,19 +18,5 @@ namespace GC
         TypedObject(Param ... params) : object(T(std::forward<Param>(params) ...)) {}
 
         T object;
-    };
-
-    template<typename T>
-    struct TypedObject<T, std::enable_if_t<std::is_array<T>::value>> : public Object
-    {
-        using type = construct_array_t<T>;
-
-        template<typename ... Param>
-        TypedObject(Param ... params)
-        {
-            object = { params ... };
-        }
-
-        construct_array_t<T> object;
     };
 }
