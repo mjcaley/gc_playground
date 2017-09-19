4a6e344b2af583805bb639ae5853d86683c69e52
Michael Caley <mjcaley@darkarctic.com>
2017-09-19 09:23:28 -0400

    Implemented some Collector functions outside of header to avoid duplicate symbols

--- include/GC/GC/Collector.hpp
+++ include/GC/GC/Collector.hpp
@@ -13,7 +13,7 @@ namespace GC
 {
     namespace Detail
     {
-        static unsigned int current_mark;
+        static unsigned int current_mark { 0 };
         static std::forward_list<Ptr> used_list;
     }
 
@@ -23,64 +23,13 @@ namespace GC
         std::uintmax_t created { 0 };
         std::uintmax_t deleted { 0 };
 
-        std::uintmax_t resolve()
-        {
-            existing += created;
-            existing -= deleted;
-            created = 0;
-            deleted = 0;
-
-            return existing;
-        }
+        std::uintmax_t resolve();
     } allocations;
 
-    static float collection_percentage { 0.25 };
+    static float collection_percentage { 0.25f };
 
-    static void collect();
-
-    static Ptr* add(Ptr pointer)
-    {
-        Detail::used_list.emplace_front(std::move(pointer));
-        ++allocations.created;
-        float p = std::min(1.0f, std::max(collection_percentage, 0.0f));
-        if (allocations.created > (allocations.existing / p))
-        {
-            collect();
-        }
-
-        return &Detail::used_list.front();
-    }
-
-    static void mark(unsigned int marker)
-    {
-        for(auto& ptr : Detail::used_list)
-        {
-            if (ptr.is_root())
-            {
-                ptr.mark(marker);
-            }
-        }
-    }
-
-    static void sweep(unsigned int marker)
-    {
-        Detail::used_list.remove_if(
-            [&marker](auto& ptr) {
-                bool result = ptr.get_mark() != marker;
-                if (result)
-                {
-                    ++allocations.deleted;
-                }
-                return result;
-            }
-        );
-    }
-
-    static void collect()
-    {
-        mark(Detail::current_mark);
-        sweep(Detail::current_mark);
-        allocations.resolve();
-        ++Detail::current_mark;
-    }
+    Ptr* add(Ptr pointer);
+    void mark(unsigned int marker);
+    void sweep(unsigned int marker);
+    void collect();
 }
