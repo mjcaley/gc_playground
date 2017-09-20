3f7fb4a46764f636afcf335bdbf6817f9b1c96f7
Michael Caley <mjcaley@darkarctic.com>
2017-09-20 10:33:09 -0400

    Added traverse_iterator function

--- include/GC/GC/Traverse.hpp
+++ include/GC/GC/Traverse.hpp
@@ -6,6 +6,7 @@
 namespace GC
 {
     template<typename T> void traverse_container(T& object, unsigned int marker);
+    template<typename Iterator> void traverse_iterator(Iterator begin, Iterator end, unsigned int marker);
     
     template<typename T>
     void traverse(T& object, unsigned int marker) {}
@@ -13,19 +14,19 @@ namespace GC
     template<typename T>
     void traverse(std::enable_if_t<std::is_array<T>::value, construct_array_t<T>>& object, unsigned int marker)
     {
-        traverse_container(object, marker);
+        traverse_iterator(std::begin(object), std::end(object), marker);
     }
 
     template<typename T, std::size_t S>
     void traverse(std::array<T, S>& object, unsigned int marker)
     {
-        traverse_container(object, marker);
+        traverse_iterator(std::begin(object), std::end(object), marker);
     }
 
     template<typename T>
     void traverse(std::vector<T>& object, unsigned int marker)
     {
-        traverse_container(object, marker);
+        traverse_iterator(std::begin(object), std::end(object), marker);
     }
 
     template<typename T>
@@ -34,6 +35,15 @@ namespace GC
         for (auto& o : object)
         {
             traverse(o, marker);
+        }
+    }
+
+    template<typename Iterator>
+    void traverse_iterator(Iterator begin, Iterator end, unsigned int marker)
+    {
+        for (; begin != end; ++begin)
+        {
+            traverse(*begin, marker);
         }
     }
 }
