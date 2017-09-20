63e015b5501ed949c2cf7ecb35df1a3257344527
Michael Caley <mjcaley@darkarctic.com>
2017-09-20 10:46:53 -0400

    Added tests for traverse functions

---
+++ test/test_traverse.cpp
@@ -0,0 +1,74 @@
+#include "catch.hpp"
+
+#include "GC/GC.hpp"
+
+#include <array>
+#include <vector>
+
+
+struct TraverseMock
+{
+    unsigned int mark { 0 };
+};
+
+template<>
+void GC::traverse(TraverseMock& object, unsigned int marker)
+{
+    object.mark = marker;
+}
+
+TEST_CASE( "Traverse passes mark", "[gc][traverse]" )
+{
+    TraverseMock obj;
+    GC::traverse(obj, 42);
+
+    REQUIRE(obj.mark == 42);
+}
+
+TEST_CASE( "Traverse iterator of Mocks", "[gc][traverse]" )
+{
+    std::vector<TraverseMock> mocks(10);
+
+    GC::traverse_iterator(std::begin(mocks), std::end(mocks), 42);
+
+    for (auto& mock : mocks)
+    {
+        REQUIRE(mock.mark == 42);
+    }
+}
+
+TEST_CASE( "Traverse vector container of Mocks", "[gc][traverse]" )
+{
+    std::vector<TraverseMock> mocks(10);
+
+    GC::traverse_container(mocks, 42);
+
+    for (auto& mock : mocks)
+    {
+        REQUIRE(mock.mark == 42);
+    }
+}
+
+TEST_CASE( "Traverse array container of Mocks", "[gc][traverse]" )
+{
+    std::array<TraverseMock, 10> mocks;
+
+    GC::traverse_container(mocks, 42);
+
+    for (auto& mock : mocks)
+    {
+        REQUIRE(mock.mark == 42);
+    }
+}
+
+TEST_CASE( "Traverse C array of Mocks", "[gc][traverse]" )
+{
+    std::array<TraverseMock, 10> mocks;
+
+    GC::traverse<TraverseMock[10]>(mocks, 42);
+
+    for (auto& mock : mocks)
+    {
+        REQUIRE(mock.mark == 42);
+    }
+}
