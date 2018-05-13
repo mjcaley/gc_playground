dd46f49e5f561c8b7114b36181b80778f6137071
Michael Caley <mjcaley@darkarctic.com>
2018-05-12 23:14:48 -0400

    Revert "Revert "Moved mark and sweep functions to gc.hpp""
    This reverts commit 64a13f89f4be5e14e8bff5d68857c9d8863b15fc.

--- src/new_demo/main.cpp
+++ src/new_demo/main.cpp
@@ -13,43 +13,8 @@
 #include "frame.hpp"
 #include "memory.hpp"
 #include "types.hpp"
+#include "gc.hpp"
 
-
-
-std::vector<PointerBase*> get_roots(Frame* frame)
-{
-    std::vector<PointerBase*> roots;
-    while (frame)
-    {
-        auto locals = frame->get_locals();
-        roots.insert(std::end(roots),
-                     std::begin(locals),
-                     std::end(locals));
-        frame = frame->get_next();
-    }
-    
-    return roots;
-}
-
-int current = 0;
-
-void mark(Frame& frame)
-{
-    ++current;
-    auto roots = get_roots(&frame);
-    for (auto& root : roots)
-    {
-        root->mark(current);
-    }
-}
-
-template<typename T>
-void sweep(T& allocations)
-{
-    allocations.remove_if(
-        [](const Allocation& a) { return a.mark != current; }
-    );
-}
 
 
 int num_allocations(MemoryManager& memory)
